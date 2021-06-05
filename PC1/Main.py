56b020e8a4764c14ae44bffdd053c53ddd8baccf
Rafael Antunes <rafaelantunesvieira@outlook.com.br>
2021-06-05 15:14:18 -0300

    Atualizando codigos

--- PC1/Main.py
+++ PC1/Main.py
@@ -6,14 +6,14 @@ x = 1
 
 # Define IP  e Porta
 HOST = 'localhost'
-PORT = 57000
+PORT = 1111
 
 # Faz a configuração do Socket com os protocolos IPV4/TCP e configura o IP e Porta
 ObjSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
 def servidor():
-    # Menu provavelmente vem aqui
-    
+
     ObjSocket.bind((HOST, PORT))
     print("SERVIDOR ATIVO!\nEsperando conexao...")
 
@@ -27,44 +27,7 @@ def servidor():
     # Cliente, neste ponto, estara enviando o nome do arquivo solicitado
     resposta = conexao.recv(1024)
 
-    # Abro o arquivo que vou enviar
-    arq = open(resposta.decode(), 'rb')
-    print("Enviando o arquivo...")
-
-    # Envio o arquivo solicitado
-    while 1:
-        dados = conexao.recv(1024)
-        if not dados:
-            break
-        arq.write(dados)
-    
-    # Fecha o arquivo
-    arq.close()
-
-# def encerrar(arq, conexao):
-    # Fecho o arquivo
-    # arq.close()
-    # Fecho a conexao
-    # conexao.close()
-
-def cliente():
-    # Abro o arquivo
-    print ("\nInsira o nome (com extensão) do arquivo que deseja receber\n")
-    nome_arquivo = input()
-
-    # Procura no tracker
-    # Tracker retorna quem tem, lê host e porta
-
-    # Estabelecer conexão com esse PC
-
-    # Receber o arquivo
-
-    # Acrescentar no tracker que ele tem o arquivo
-
-    # Faço a conexao no IP e Porta
-    ObjSocket.connect((HOST, PORT))
-
-    arq = open(nome_arquivo, 'rb')
+    arq = open(resposta, 'rb')
 
     # Mando o arquivo
     ObjSocket.sendfile(arq)
@@ -72,21 +35,67 @@ def cliente():
     # Fecho o arquivo
     arq.close()
 
-    # Depois de executar atividade de cliente, restaura status de servidor desta maquina
-    servidor()
-    
+
+def cliente():
+    # Abro o arquivo
+    print("\nInsira o nome (com extensão) do arquivo que deseja receber\n")
+    nome_arquivo = input()
+
+    # Procura no tracker
+    tracker = open('tracker.txt', 'r')
+    print("\nPesquisando no tracker.....")
+
+    # Executa um loop dentro do arquivo
+    for line in tracker:
+        # Le a linha do arquivo
+        linha = line.readline()
+        # Se essa linah tem o arquivo que preciso....
+        if(linha.split(0) == nome_arquivo):
+            # Tracker retorna quem tem, lê host e porta
+            hostArq = linha.split(1)
+            portArq = linha.split(2)
+            # Faço a conexao no IP e Porta
+            # Estabelecer conexão com esse PC
+            try:
+                ObjSocket.connect((hostArq, portArq))
+                print('Conexão concluida!\n')
+                ObjSocket.sendall(str.encode(nome_arquivo))
+                # Receber o arquivo
+                # Envio o arquivo solicitado
+                while 1:
+                    dados = ObjSocket.recv(1024)
+                    if not dados:
+                        break
+                # Salvar o arquivo
+                arq = open(nome_arquivo, 'wb')
+                arq.write(dados)
+                arq.close()
+                # Atualiza o tracker
+                # Fecho o tracker em modo de leitura
+                tracker.close()
+                # Abro o tracker em modo de atualização
+                tracker = open('tracker.txt', 'a')
+                print("\nAtualizando o tracker.....")
+                tracker.write(nome_arquivo+' '+HOST+' '+PORT+'\n')
+                tracker.close()
+
+            except socket.error:
+                print('\nConexão indisponivel, tentando outra conexao')
+
 
 # Este while funciona como a main do PC. PC é setado sempre como servidor
 # para que possa ficar sempre disponivel para transfeir arquivo
 
 while x:
-    servidor()
     print("====================================================")
-    print("\n -> Pressione A para solicitar um arquivo")
+    print("\n -> Pressione C para solicitar um arquivo")
+    print("\n -> Pressione S para solicitar um arquivo")
     print("\n -> Pressione X para sair da aplicação\n")
     opcao = input()
 
-    if opcao == 'A' or opcao == 'a':
+    if opcao == 'C' or opcao == 'c':
         cliente()
+    elif opcao == 'S' or opcao == 's':
+        servidor()
     elif opcao == 'X' or opcao == 'x':
         x = 0
--- PC2/Main.py
+++ PC2/Main.py
@@ -1,21 +1,101 @@
-# client_sock.py
+# serv_sock.py
 import socket
+
+# Definicao de uma variavel x para o menu da aplicacao
+x = 1
 
 # Define IP  e Porta
 HOST = 'localhost'
-PORT = 57000
+PORT = 2222
 
 # Faz a configuração do Socket com os protocolos IPV4/TCP e configura o IP e Porta
 ObjSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Faço a conexao no IP e Porta
-ObjSocket.connect((HOST, PORT))
 
-# Abro o arquivo
-arq = open('tracker.txt', 'rb')
+def servidor():
 
-# Mando o arquivo
-ObjSocket.sendfile(arq)
+    ObjSocket.bind((HOST, PORT))
+    print("SERVIDOR ATIVO!\nEsperando conexao...")
 
-# Fecho o arquivo
-arq.close()
+    # Ouve a conexao na porta
+    ObjSocket.listen(1)
+
+    # Confirmo a conexao com o outro PC
+    conexao, endereco = ObjSocket.accept()
+    print('Conectado com:', endereco)
+
+    # Cliente, neste ponto, estara enviando o nome do arquivo solicitado
+    resposta = conexao.recv(1024)
+
+    arq = open(resposta, 'rb')
+
+    # Mando o arquivo
+    ObjSocket.sendfile(arq)
+
+    # Fecho o arquivo
+    arq.close()
+
+
+def cliente():
+    # Abro o arquivo
+    print("\nInsira o nome (com extensão) do arquivo que deseja receber\n")
+    nome_arquivo = input()
+
+    # Procura no tracker
+    tracker = open('tracker.txt', 'r')
+    print("\nPesquisando no tracker.....")
+
+    # Executa um loop dentro do arquivo
+    for line in tracker:
+        # Le a linha do arquivo
+        linha = line.readline()
+        # Se essa linah tem o arquivo que preciso....
+        if(linha.split(0) == nome_arquivo):
+            # Tracker retorna quem tem, lê host e porta
+            hostArq = linha.split(1)
+            portArq = linha.split(2)
+            # Faço a conexao no IP e Porta
+            # Estabelecer conexão com esse PC
+            try:
+                ObjSocket.connect((hostArq, portArq))
+                print('Conexão concluida!\n')
+                ObjSocket.sendall(str.encode(nome_arquivo))
+                # Receber o arquivo
+                # Envio o arquivo solicitado
+                while 1:
+                    dados = ObjSocket.recv(1024)
+                    if not dados:
+                        break
+                # Salvar o arquivo
+                arq = open(nome_arquivo, 'wb')
+                arq.write(dados)
+                arq.close()
+                # Atualiza o tracker
+                # Fecho o tracker em modo de leitura
+                tracker.close()
+                # Abro o tracker em modo de atualização
+                tracker = open('tracker.txt', 'a')
+                print("\nAtualizando o tracker.....")
+                tracker.write(nome_arquivo+' '+HOST+' '+PORT+'\n')
+                tracker.close()
+
+            except socket.error:
+                print('\nConexão indisponivel, tentando outra conexao')
+
+
+# Este while funciona como a main do PC. PC é setado sempre como servidor
+# para que possa ficar sempre disponivel para transfeir arquivo
+
+while x:
+    print("====================================================")
+    print("\n -> Pressione C para solicitar um arquivo")
+    print("\n -> Pressione S para Servidor")
+    print("\n -> Pressione X para sair da aplicação\n")
+    opcao = input()
+
+    if opcao == 'C' or opcao == 'c':
+        cliente()
+    elif opcao == 'S' or opcao == 's':
+        servidor()
+    elif opcao == 'X' or opcao == 'x':
+        x = 0
